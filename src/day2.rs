61f9d2766b8f5885844fe3a21827a65c78dd1e05
Thijs Koot <thijs@thijskoot.nl>
2022-12-02 22:22:59 +0100

    day 2 refactor for you

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, str::FromStr};
 
 pub fn advent_2() {
     let score1: i32 = fs::read_to_string("inputs/day2")
@@ -18,27 +18,27 @@ pub fn advent_2() {
 }
 
 struct FixedBattle {
-    opp: RockPaperScissor,
+    opp: Choice,
     outcome: Outcome,
 }
 
 impl FixedBattle {
     fn as_battle(&self) -> Battle {
         let your_move = match self.opp {
-            RockPaperScissor::Rock => match self.outcome {
-                Outcome::Win => RockPaperScissor::Paper,
-                Outcome::Draw => RockPaperScissor::Rock,
-                Outcome::Lose => RockPaperScissor::Scissor,
+            Choice::Rock => match self.outcome {
+                Outcome::Win => Choice::Paper,
+                Outcome::Draw => Choice::Rock,
+                Outcome::Lose => Choice::Scissor,
             },
-            RockPaperScissor::Paper => match self.outcome {
-                Outcome::Win => RockPaperScissor::Scissor,
-                Outcome::Draw => RockPaperScissor::Paper,
-                Outcome::Lose => RockPaperScissor::Rock,
+            Choice::Paper => match self.outcome {
+                Outcome::Win => Choice::Scissor,
+                Outcome::Draw => Choice::Paper,
+                Outcome::Lose => Choice::Rock,
             },
-            RockPaperScissor::Scissor => match self.outcome {
-                Outcome::Win => RockPaperScissor::Rock,
-                Outcome::Draw => RockPaperScissor::Scissor,
-                Outcome::Lose => RockPaperScissor::Paper,
+            Choice::Scissor => match self.outcome {
+                Outcome::Win => Choice::Rock,
+                Outcome::Draw => Choice::Scissor,
+                Outcome::Lose => Choice::Paper,
             },
         };
 
@@ -58,25 +58,25 @@ impl TryFrom<&str> for FixedBattle {
         if inputs.len() != 2 {
             Err("should have two elements")
         } else {
-            let opp = RockPaperScissor::try_from(inputs[0])?;
-            let outcome = Outcome::try_from(inputs[1])?;
-
-            Ok(FixedBattle { opp, outcome })
+            Ok(FixedBattle {
+                opp: Choice::from_str(inputs[0])?,
+                outcome: Outcome::from_str(inputs[1])?,
+            })
         }
     }
 }
 
 struct Battle {
-    you: RockPaperScissor,
-    opp: RockPaperScissor,
+    you: Choice,
+    opp: Choice,
 }
 
 impl Battle {
     pub fn calc_points(&self) -> i32 {
         let shape_points = match self.you {
-            RockPaperScissor::Rock => 1,
-            RockPaperScissor::Paper => 2,
-            RockPaperScissor::Scissor => 3,
+            Choice::Rock => 1,
+            Choice::Paper => 2,
+            Choice::Scissor => 3,
         };
 
         let outcome = Self::resolve(self);
@@ -92,22 +92,39 @@ impl Battle {
 
     fn resolve(&self) -> Outcome {
         match self.you {
-            RockPaperScissor::Paper => match self.opp {
-                RockPaperScissor::Rock => Outcome::Win,
-                RockPaperScissor::Paper => Outcome::Draw,
-                RockPaperScissor::Scissor => Outcome::Lose,
+            Choice::Paper => match self.opp {
+                Choice::Rock => Outcome::Win,
+                Choice::Paper => Outcome::Draw,
+                Choice::Scissor => Outcome::Lose,
             },
-            RockPaperScissor::Rock => match self.opp {
-                RockPaperScissor::Rock => Outcome::Draw,
-                RockPaperScissor::Paper => Outcome::Lose,
-                RockPaperScissor::Scissor => Outcome::Win,
+            Choice::Rock => match self.opp {
+                Choice::Rock => Outcome::Draw,
+                Choice::Paper => Outcome::Lose,
+                Choice::Scissor => Outcome::Win,
             },
-            RockPaperScissor::Scissor => match self.opp {
-                RockPaperScissor::Rock => Outcome::Lose,
-                RockPaperScissor::Paper => Outcome::Win,
-                RockPaperScissor::Scissor => Outcome::Draw,
+            Choice::Scissor => match self.opp {
+                Choice::Rock => Outcome::Lose,
+                Choice::Paper => Outcome::Win,
+                Choice::Scissor => Outcome::Draw,
             },
         }
+    }
+}
+
+impl FromStr for Battle {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let choices: Vec<Choice> = s.split(" ").map(|x| Choice::from_str(x).unwrap()).collect();
+
+        if choices.len() != 2 {
+            return Err("should have two elements");
+        }
+
+        Ok(Battle {
+            you: choices[1],
+            opp: choices[0],
+        })
     }
 }
 
@@ -115,9 +132,9 @@ impl TryFrom<&str> for Battle {
     type Error = &'static str;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let choices: Vec<RockPaperScissor> = value
+        let choices: Vec<Choice> = value
             .split(" ")
-            .map(|x| RockPaperScissor::try_from(x).unwrap())
+            .map(|x| Choice::from_str(x).unwrap())
             .collect();
 
         if choices.len() != 2 {
@@ -132,20 +149,20 @@ impl TryFrom<&str> for Battle {
 }
 
 #[derive(Clone, Copy)]
-enum RockPaperScissor {
+enum Choice {
     Rock,
     Paper,
     Scissor,
 }
 
-impl TryFrom<&str> for RockPaperScissor {
-    type Error = &'static str;
+impl FromStr for Choice {
+    type Err = &'static str;
 
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        match value {
-            "A" | "X" => Ok(RockPaperScissor::Rock),
-            "B" | "Y" => Ok(RockPaperScissor::Paper),
-            "C" | "Z" => Ok(RockPaperScissor::Scissor),
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Choice::Rock),
+            "B" | "Y" => Ok(Choice::Paper),
+            "C" | "Z" => Ok(Choice::Scissor),
             _ => Err("not valid input"),
         }
     }
@@ -157,11 +174,11 @@ enum Outcome {
     Lose,
 }
 
-impl TryFrom<&str> for Outcome {
-    type Error = &'static str;
+impl FromStr for Outcome {
+    type Err = &'static str;
 
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        match value {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
             "X" => Ok(Outcome::Lose),
             "Y" => Ok(Outcome::Draw),
             "Z" => Ok(Outcome::Win),
